0eac290871244075f5f8fcb6c31c23bd1657b0d0
Madison Gessner <madagessner@gmail.com>
2022-01-12 15:57:22 -0600

    Making a new repository

---
+++ src/index.js
@@ -0,0 +1,56 @@
+document.addEventListener('DOMContentLoaded',() =>{
+    //card options
+    const cards = [
+        {
+            name: 'fries',
+            img: 'src/images/fries.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'src/images/cheeseburger.png'
+        },
+        {
+            name: 'icecream',
+            img: 'src/images/icecream.png'
+        },
+        {
+            name: 'pizza',
+            img: 'src/images/pizza.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'src/images/milkshake.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'src/images/hotdog.png'
+        },
+
+        {
+            name: 'fries',
+            img: 'src/images/fries.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'src/images/cheeseburger.png'
+        },
+        {
+            name: 'icecream',
+            img: 'src/images/icecream.png'
+        },
+        {
+            name: 'pizza',
+            img: 'src/images/pizza.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'src/images/milkshake.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'src/images/hotdog.png'
+        },
+    ]
+
+    console.log(cards)
+})
